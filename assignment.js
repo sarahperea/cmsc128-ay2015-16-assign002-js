bd76bf6290cfbcdba7bad1eaf564e3f95a60f9b2
Sarah D. Perea <sdperea.1@gmail.com>
2019-07-02 14:56:45 +0800

    Move getSkew.js

--- assignment.js
+++ assignment.js
@@ -1,22 +1,3 @@
-function getSkew(string str, int n){ //returns an integer
-//Given a genome str of some length q (where q>0), it returns the number of Gs minus the number of Cs
-//in the first n nucleotides (q>=n). The value can be zero, negative or positive. The first position is
-//one (1) not zero(0) as we typically associate with string implementations. (description from the handout)  
-	var G = 0; //variable for counting the number of G's from index 0 to index n-1
-	var C = 0; //variable for counting the number of C's from index 0 to index n-1
-	
-	//increments the number of G's or C's if they are found in str
-	for (var i=0; i<n; i++){
-		if (str[i]=='G'){
-			G++;
-		}else if (str[i]=='C'){
-			C++;
-		}
-	}
-	return G-C; //returns number of G's minus C's
-}
-
-//----------------------------------------------------------------------------------------------------
 function getMaxSkewN(string str, int n){ //returns an integer
 //Given a genome str of some length q (where q>0), it returns the maximum value of the number of Gs minus the
 //number of Cs in the first n nucleotides (q>=n). The value can be zero, negative or positive. The first position
