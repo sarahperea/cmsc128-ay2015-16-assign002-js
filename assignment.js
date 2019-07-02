4cf60f57403d6b69f0484509ba50fd6383b9a867
Sarah D. Perea <sdperea.1@gmail.com>
2019-07-02 14:52:25 +0800

    Move countSubstrPattern

--- assignment.js
+++ assignment.js
@@ -1,25 +1,3 @@
-function countSubstrPattern(string original, string pattern){
-//This function counts the occurrence of pattern in the original string
-	var patternOccurrence=0;
-	var numOfSameChar=0;
-
-	for (var i=0; var<original.length; i++){
-		for (var j=0, k=i; j<pattern.length; j++){
-			//counts and compare the number of same characters of original and pattern strings
-			if (original[k] == pattern[j]){
-				numOfSameChar++;
-			}
-			k++; //moves to next char of original string
-		}
-		//if the pattern is found starting from index i of the original
-		if (numOfSameChar == pattern.length){
-			patternOccurrence++; //increments the number of occurrence
-		}		
-	}
-	return patternOccurrence; //returns the num of occurrence of pattern in original string
-}
-
-//----------------------------------------------------------------------------------------------------
 function isValidString(string str, string alphabet){
 //This function returns true if the string str is a valid string based on the letters of alphabet
 	for (var i=0; i<alphabet.length; i++){
